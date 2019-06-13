35dac172baf982dd15be625e7a540f987fa050e7
nboober <nboober@terpmail.umd.edu>
2019-06-13 10:12:24 -0400

    Added functionality for if index is too large. Now if too large of an index is entered you will be prompted to enter another one until the index is smaller then the word length

--- src/StringMethods.java
+++ src/StringMethods.java
@@ -6,13 +6,22 @@ public class StringMethods {
         String word;
         int index;
 
-        System.out.println("Enter a Word: ");
-        word = scanner.next();
+            System.out.println("Enter a Word: ");
+            word = scanner.next();
 
-        System.out.println("Enter a character number index: ");
-        index = scanner.nextInt();
+            System.out.println("Enter a character number index: ");
+            index = scanner.nextInt();
 
-        System.out.println("Your word \"" + word + "\" is " + word.length() + " characters long.");
-        System.out.println("The Substring of \"" + word + "\" from 0 to index " + index + " is \"" + word.substring(0,index) + "\"");
+            while(index > word.length()){
+                System.out.println("Your word \"" + word + "\" is " + word.length() + " characters long.");
+                System.out.println("The index you selected is too large. Please enter a smaller index: ");
+                index = scanner.nextInt();
+            }
+
+        if(index < word.length()){
+            System.out.println("Your word \"" + word + "\" is " + word.length() + " characters long.");
+            System.out.println("The Substring of \"" + word + "\" from 0 to index " + index + " is \"" + word.substring(0, index) + "\"");
+        }
+
     }
 }
